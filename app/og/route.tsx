040908d3d596890de8c9e4308bd0bd3ab69dbbe1
kenmwangi <kenmwangi071@gmail.com>
2024-04-13 11:51:20 +0300

    open-graph

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,22 +1,84 @@
 import { ImageResponse } from "next/og";
+import { NextRequest } from "next/server";
 
-export function GET(req: Request) {
-  let url = new URL(req.url);
-  let title = url.searchParams.get("title") || "Ken Mwangi";
+// Route segment config
+export const runtime = "edge";
 
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const postTitle = searchParams.get("title") || "Ken Mwangi";
+
+  const font = fetch(
+    new URL("../../../public/fonts/Aeonik-Bold.otf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  const fontData = await font;
+
+  //   Create an ImageResponse with dynamic content
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
-          </h2>
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          backgroundColor: "white",
+          backgroundImage:
+            "radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)",
+          backgroundSize: "100px 100px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            fontSize: 140,
+            fontStyle: "normal",
+            color: "black",
+            marginTop: 30,
+            lineHeight: 1.8,
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          <b>{postTitle}</b>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: 50,
+          }}
+        >
+          @ken_cipher
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "#1066ef",
+            fontSize: 40,
+          }}
+        >
+          Software Engineer | Tech Enthusiast | Value Creator
         </div>
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 1920,
+      height: 1080,
+      fonts: [
+        {
+          name: "Aeonik",
+          data: fontData,
+          style: "normal",
+        },
+      ],
     }
   );
 }
