a8ddc5ac7dcacd39c4bb16a5b6c39fc867c9d86b
kenmwangi <kenmwangi071@gmail.com>
2025-03-04 13:41:05 +0300

    added Dockerfiles"

--- apps/web/app/products/products.tsx
+++ apps/web/app/products/products.tsx
@@ -1,20 +1,19 @@
 import { Product } from "@repo/types";
+
 export default async function Products() {
   const response = await fetch(`${process.env.API_URL}/products`, {
     next: { tags: ["products"] },
   });
-
   const products: Product[] = await response.json();
 
   return (
     <div>
       <h1>Products</h1>
-
       <div>
         {products.map((product) => (
           <div key={product.id}>
-            <h2>Name: {product.name}</h2>
-            <strong>Price: ${product.price}</strong>
+            <p>Name: {product.name}</p>
+            <p>Price: {product.price}</p>
           </div>
         ))}
       </div>
